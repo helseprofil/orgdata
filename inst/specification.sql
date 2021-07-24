6bd63619cad7e2dce55a456540f85d0e87f38165
YBK <ybkamaleri@users.noreply.github.com>
2021-07-24 23:59:54 +0200

    Style: Reindent SQL code

--- inst/specification.sql
+++ inst/specification.sql
@@ -1,8 +1,8 @@
 SELECT KOBLID, tbl_Koble.FILID, tbl_Koble.FILGRUPPE, FILNAVN, IBRUKTIL, tbl_Innlesing.*
 FROM tbl_Innlesing
 INNER JOIN (tbl_Koble
-INNER JOIN tbl_Orgfile
-ON tbl_Koble.FILID = tbl_Orgfile.FILID)
+            INNER JOIN tbl_Orgfile
+            ON tbl_Koble.FILID = tbl_Orgfile.FILID)
 ON (tbl_Innlesing.LESID = tbl_Koble.LESID)
 WHERE tbl_Koble.FILGRUPPE = '%s'
 AND tbl_Orgfile.IBRUKTIL = #9999-01-01#
