d9e32fa073486ca3159379dce4e5b35f92bf4ec6
YBK <ybkamaleri@users.noreply.github.com>
2021-07-26 01:21:08 +0200

    Feat: Get input col with multiple args
    This is when the column input in the database has multiple
    arguments separeted by "," etc eg. INNLESARG and EXTRA column.

---
+++ R/innlesing.R
@@ -0,0 +1,26 @@
+#' Get File Specifications
+#'
+#' @description
+#' This function will access all specifications on how
+#' the data will be clean, restructured and aggregated. These specifications are registered in
+#' the following register database tables:
+#' \enumerate{
+#'   \item{tbl_Filgruppe}
+#'   \item{tbl_Orgfile}
+#'   \item{tbl_Innlesing}
+#' }
+#'
+#' @param arg Name of the arguments
+#' @param col Column name in the table
+#' @param ... Other arguments eg. sep = "|"
+#' @examples
+#' \dontrun{
+#' input <- get_innlesarg("ark", spec$INNLESARG)
+#' }
+#' @return A character
+#' @export
+
+get_innlesarg <- function(arg, col, ...) {
+  args <- get_column_arg(col, ...)
+  input <- get_input_arg(arg, args)
+}
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,4 @@
+# Check code ------------------------------------------
 # SQL code need sprintf for dynamic query
 check_sql <- function(x) {
   if (grepl("%", x) != 1) {
@@ -5,10 +6,35 @@ check_sql <- function(x) {
   }
 }
 
-
+# Arguments that are missing
 check_null <- function(arg) {
   argchr <- deparse(substitute(arg))
   if (is.null(arg)) {
     stop("Argument for ", argchr, " is missing")
   }
 }
+
+
+# Get argument input ------------------------------------------
+# When DB column has many input e.g INNLESARG and EXTRA column
+get_column_arg <- function(col = NULL, sep = c(",", "|")) {
+  # col : colum name in Access DB
+  # sep = , : INNLESARG
+  # sep = | : EXTRA
+  # Output is a vector of the INNLESARG input
+  args <- unlist(strsplit(col, sep))
+  args <- trimws(args)
+}
+
+# Example
+## (arr <- get_column_input(spec$INNLESARG))
+
+get_input_arg <- function(arg = NULL, y = NULL) {
+  # arg : Name of arg in the column eg. header
+  # y : the set of arguments in the columns INNLESARG as a vector via get_column_arg()
+  i <- grep(paste0("^", arg), y)
+  input <- unlist(strsplit(y[i], "="))[2]
+}
+
+## Example
+## arg <- get_input_arg("ark", args)
