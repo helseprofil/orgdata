df6d15cb10d16ebea15ba0ba6ac170e39461ea25
YBK <ybkamaleri@users.noreply.github.com>
2021-07-26 17:06:19 +0200

    Test: TRUE FALSE is logical

--- tests/testthat/test-innlesing.R
+++ tests/testthat/test-innlesing.R
@@ -1,7 +1,11 @@
 test_that("Get input column", {
-  txt1 <- "header=TRUE, skip=TRUE"
+  txt1 <- "header=TRUE, ark=Sheet1"
   txt2 <- "header=TRUE : sep=,"
+  txt3 <- get_column_multi_args(txt1)
 
-  expect_equal(get_column_multi_args(txt1), c("header=TRUE", "skip=TRUE"))
+  expect_equal(get_column_multi_args(txt1), c("header=TRUE", "ark=Sheet1"))
   expect_equal(get_column_multi_args(txt2, sep = ":"), c("header=TRUE", "sep=,"))
+
+  expect_equal(get_input_multi_args("ark", txt3), "Sheet1")
+  expect_equal(get_input_multi_args("header", txt3), TRUE)
 })
