d1bd250be0c8c9ac4211147a5d7e7ac358093e58
YBK <ybkamaleri@users.noreply.github.com>
2021-07-26 16:30:39 +0200

    Feat: Change function name for col multi args
    This is only for Access columns with possibility to add
    multiple arguments.

--- R/utils.R
+++ R/utils.R
@@ -17,24 +17,26 @@ check_null <- function(arg) {
 
 # Get argument input ------------------------------------------
 # When DB column has many input e.g INNLESARG and EXTRA column
-get_column_arg <- function(col = NULL, sep = c(",", "|")) {
+get_column_multi_args <- function(col = NULL, sep = c(",", ":", ";")) {
   # col : colum name in Access DB
   # sep = , : INNLESARG
   # sep = | : EXTRA
   # Output is a vector of the INNLESARG input
+  check_null(col)
+  sep <- match.arg(sep)
   args <- unlist(strsplit(col, sep))
   args <- trimws(args)
 }
-
 # Example
-## (arr <- get_column_arg(spec$INNLESARG))
+## (arr <- get_column_multi_args(spec$INNLESARG))
 
-get_input_arg <- function(arg = NULL, y = NULL) {
+get_input_multi_args <- function(arg = NULL, y = NULL) {
   # arg : Name of arg in the column eg. header
-  # y : the set of arguments in the columns INNLESARG as a vector via get_column_arg()
+  # y : the set of arguments in the columns INNLESARG as a vector via get_column_multi_args()
+  check_null(arg)
+  check_null(y)
   i <- grep(paste0("^", arg), y)
   input <- unlist(strsplit(y[i], "="))[2]
 }
-
 ## Example
-## arg <- get_input_arg("ark", arr)
+## arg <- get_input_multi_args("ark", arr)
