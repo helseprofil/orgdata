39512ef984442be82adfd61cee68af18cda163b0
YBK <ybkamaleri@users.noreply.github.com>
2021-07-28 15:04:59 +0200

    Test: Add dataset for testing

--- tests/testthat/setup-connection.R
+++ tests/testthat/setup-connection.R
@@ -4,3 +4,18 @@
 ## --------------------------------------------------------------------
 ## dbfile <- "N:/Helseprofiler/DB_helseprofil/org-innlesing.accdb"
 ## kh <- KHelse$new(dbfile)
+
+## Dataset 01 ---------------------------------------------------------
+df01 <- structure(list(
+  KOBLID = 20L, FILID = 15L, FILGRUPPE = "TEST01",
+  FILNAVN = "Path/to/testfile.csv",
+  IBRUKTIL = structure(253370764800, class = c(
+    "POSIXct",
+    "POSIXt"
+  ), tzone = "UTC"),
+  LESID = 18L, FILGRUPPE = "TEST01",
+  INNLESARG = "header=TRUE, skip=TRUE, ark=Sheet1", MANHEADER = "2,3=KJONN,ALDER",
+  GEO = NA_character_, AAR = "aar", KJONN = NA_character_, ALDER = NA_character_,
+  UTDANN = NA_character_, LANDBAK = NA_character_, VAL = NA_character_,
+  EXTRA = "sep=,"
+), class = "data.frame", row.names = c(NA, -1L))
--- tests/testthat/test-innlesing.R
+++ tests/testthat/test-innlesing.R
@@ -1,17 +1,3 @@
 test_that("Year for data", {
-  df <- structure(list(
-    KOBLID = 20L, FILID = 15L, FILGRUPPE = "TEST01",
-    FILNAVN = "Path/to/testfile.csv",
-    IBRUKTIL = structure(253370764800, class = c(
-      "POSIXct",
-      "POSIXt"
-    ), tzone = "UTC"),
-    LESID = 18L, FILGRUPPE = "TEST01",
-    INNLESARG = "header=TRUE, skip=TRUE, ark=Sheet1", MANHEADER = "2,3=KJONN,ALDER",
-    GEO = NA_character_, AAR = "aar", KJONN = NA_character_, ALDER = NA_character_,
-    UTDANN = NA_character_, LANDBAK = NA_character_, VAL = NA_character_,
-    EXTRA = "sep=,"
-  ), class = "data.frame", row.names = c(NA, -1L))
-
-  expect_identical(get_year(df), "aar")
+  expect_identical(get_year(df01), "aar")
 })
