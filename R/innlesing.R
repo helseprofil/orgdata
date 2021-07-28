6af3624e3c1667646b1347415625d9a30cf046b0
YBK <ybkamaleri@users.noreply.github.com>
2021-07-28 22:51:07 +0200

    Feat: Use only df for get_innlesarg

--- R/innlesing.R
+++ R/innlesing.R
@@ -1,20 +1,19 @@
 #' Get INNLESARG Specifications
 #'
 #' @description
-#' This function will access all arguments in columns \code{INNLESARG} in table
-#' \emph{tbl_Innlesing}' where there are possibility to
-#' specify multiple arguments for example:
+#' This function will access all possible arguments in columns \code{INNLESARG}
+#' in table \emph{tbl_Innlesing}'. Comma \code{","} \strong{MUST} be used to
+#' seperate arguments if there are more than one argument.
+#' For example:
+#'
 #' `header=TRUE, ark=Sheet1`
-#' @param arg Name of the arguments
-#' @param ... Other arguments eg. sep = ":"
-#' @inheritParams find_column_input
+#' @inheritParams get_year
+#' @inheritParams find_spec
 #' @examples
-#' \dontrun{
-#' input <- get_innlesarg("ark", spec$INNLESARG)
-#' }
-#' @return A character
+#' @return A list with the names and value of arguments
 #' @export
-get_innlesarg <- function(arg, col, ...) {
-  args <- find_column_multi(col, ...)
-  input <- find_column_multi_input(arg, args)
+get_innlesarg <- function(df = NULL) {
+  # column INNLESARG must use coma ',' as seperation btw arguments!
+  args <- find_column_multi(df, "INNLESARG", sep = ",")
+  input <- find_column_multi_input(args)
 }
--- R/utils-multi-input.R
+++ R/utils-multi-input.R
@@ -1,40 +1,99 @@
 # Get argument input ------------------------------------------
 # When DB column has many input e.g INNLESARG and EXTRA column
 #' @title Columns with Multiple Inputs
-#' @description Get all arguments in the columns that have multiple arguments with [find_column_multi()].
-#' The output will be a vector that can be used in [orgdata::find_column_multi_input()] to get the value of the input.
+#' @description Get all arguments in a selected column that have multiple
+#' arguments with [find_column_multi()]. The output will be a character
+#' type of arguments length. This can then can be used in [find_column_multi_input()]
+#' function to get the value of the input as a list object. If you are only
+#' interested in a specific argument among these arguments in the column,
+#' then use [find_column_multi_input_arg()] function. See example.
 #' @param sep Symbols that seperate these arguments eg. "," or ":"
 #' @inheritParams find_column_input
 #' @examples
 #' \dontrun{
 #' args <- find_column_multi(spec$INNLESARG)
-#' val <- find_column_multi_input("header", args)
+#' vals <- find_column_multi_input(args)
+#' val <- find_column_multi_input_arg(args, "header")
 #' }
-#' @return A vector of the argument(s) that is seperated with \code{sep} argument
+#' @return Two different output:
+#' \itemize{
+#'    \item{A vector of the argument that is seperated with \code{sep} argument}
+#'    \item{A list of argument names and their values}
+#' }
 #' @export
-find_column_multi <- function(col = NULL, sep = c(",", ":", ";")) {
+find_column_multi <- function(df = NULL, col = NULL, sep = c(",", ":", ";")) {
   # Output is a vector of the INNLESARG input
-  check_null(col)
+  check_null(df)
   sep <- match.arg(sep)
-  args <- unlist(strsplit(col, sep))
-  args <- trimws(args)
+
+  if (is.null(col)) {
+    args <- seperate_value(df, sep)
+  } else {
+    args <- seperate_value(df[, col], sep)
+  }
+  return(args)
 }
 
 #' @export
 #' @rdname find_column_multi
-#' @param arg Name of argument
-#' @param y Argument(s) as a vector
-#' @return The value from the selected argument
-find_column_multi_input <- function(arg = NULL, y = NULL) {
-  # arg : Name of arg in the column eg. header
-  # y : the set of arguments in the columns INNLESARG as a vector via find_column_multi()
-  check_null(arg)
-  check_null(y)
-  i <- grep(paste0("^", arg), y)
-  input <- unlist(strsplit(y[i], "="))[2]
+#' @param input Input argument(s) as a vector
+find_column_multi_input <- function(input = NULL) {
+  check_null(input)
 
-  if (input %in% c("TRUE", "FALSE")) {
-    input <- as.logical(input)
+  inVar <- input[!is.na(input)]
+  outVar <- vector(mode = "list", length = length(inVar))
+
+  for (i in seq_len(length(input)))
+  {
+    arg <- seperate_value(input[i], "=")
+    names(outVar)[i] <- arg[1]
+    val <- make_logical(arg[2])
+    outVar[[i]] <- val
   }
-  input
+
+  return(outVar)
+}
+
+#' @export
+#' @rdname find_column_multi
+#' @param arg Name of a specific argument in the column
+#' @inheritParams find_column_multi_input
+#' @return The single object with value from the selected argument
+find_column_multi_input_arg <- function(input = NULL, arg = NULL) {
+  # arg : Name of arg in the column eg. header
+  # input : the set of arguments in the columns INNLESARG as a vector via find_column_multi()
+  check_null(arg)
+  check_null(input)
+  one_arg_only(arg)
+
+  i <- grep(paste0("^", arg), input)
+  input <- seperate_value(input[i], "=", 2)
+  input <- make_logical(input)
+  return(input)
+}
+
+one_arg_only <- function(x) {
+  many <- sum(grepl("=", x))
+  if (many > 1) {
+    stop("Too many arguments. Try `find_column_multi_inputs` function")
+  }
+}
+
+make_logical <- function(x) {
+  if (x %in% c("TRUE", "FALSE")) {
+    x <- as.logical(x)
+  }
+  return(x)
+}
+
+
+seperate_value <- function(x, sep = NULL, keep = NULL) {
+  # keep : Keep lhs or rhs eg. x[1] for lhs
+  out <- unlist(strsplit(x, sep))
+  if (!is.null(keep)) {
+    out <- out[keep]
+  }
+
+  out <- trimws(out)
+  return(out)
 }
