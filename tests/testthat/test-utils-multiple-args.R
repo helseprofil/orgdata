087b45650de4afc408360bbf06d3564b0a4d7687
YBK <ybkamaleri@users.noreply.github.com>
2021-07-28 22:53:15 +0200

    Test: Column with multiple inputs

--- tests/testthat/test-utils-multiple-args.R
+++ tests/testthat/test-utils-multiple-args.R
@@ -1,13 +1,22 @@
+test_that("Helper functions", {
+  txt1 <- "ark=Sheet1"
 
+  expect_equal(seperate_value(txt1, "="), c("ark", "Sheet1"))
+  expect_equal(seperate_value(txt1, "=", 1), "ark")
+  expect_equal(seperate_value(txt1, "=", 2), "Sheet1")
+})
 
 test_that("Get input column", {
   txt1 <- "header=TRUE, ark=Sheet1"
   txt2 <- "header=TRUE : sep=,"
   txt3 <- find_column_multi(txt1)
+  txt4 <- list(header = TRUE, ark = "Sheet1")
 
-  expect_equal(find_column_multi(txt1), c("header=TRUE", "ark=Sheet1"))
-  expect_equal(find_column_multi(txt2, sep = ":"), c("header=TRUE", "sep=,"))
+  expect_equal(find_column_multi(df = df01, col = "INNLESARG", sep = ","), c("header=TRUE", "skip=TRUE", "ark=Sheet1"))
+  expect_equal(find_column_multi(df = txt2, sep = ":"), c("header=TRUE", "sep=,"))
 
-  expect_equal(find_column_multi_input("ark", txt3), "Sheet1")
-  expect_equal(find_column_multi_input("header", txt3), TRUE)
+  expect_equal(find_column_multi_input(txt3), txt4)
+
+  expect_equal(find_column_multi_input_arg(txt3, "ark"), "Sheet1")
+  expect_equal(find_column_multi_input_arg(txt3, "header"), TRUE)
 })
