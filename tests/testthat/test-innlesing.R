79b905cc465f0fa9d38c8cf9978161d83a3a92b5
YBK <ybkamaleri@users.noreply.github.com>
2021-07-29 12:27:52 +0200

    Test: Innlesarg output as list

--- tests/testthat/test-innlesing.R
+++ tests/testthat/test-innlesing.R
@@ -1,3 +1,11 @@
+## INNLESARG -------------------------
+test_that("Innlesarg inputs", {
+  txt4 <- list(header = TRUE, skip = TRUE, ark = "Sheet1")
+
+  expect_equal(get_innlesarg(df01), txt4)
+})
+
+## Year ------------------------------
 test_that("Year for data", {
   expect_identical(get_year(df01), "aar")
 })
