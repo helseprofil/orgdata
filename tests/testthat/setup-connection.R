5a50f547131074ff490234fe20d059804ec1b354
YBK <ybkamaleri@users.noreply.github.com>
2021-07-29 17:24:17 +0200

    Test: Add test data.frame

--- tests/testthat/setup-connection.R
+++ tests/testthat/setup-connection.R
@@ -19,3 +19,19 @@ df01 <- structure(list(
   UTDANN = NA_character_, LANDBAK = NA_character_, VAL = NA_character_,
   EXTRA = "sep=,"
 ), class = "data.frame", row.names = c(NA, -1L))
+
+
+## Has error for MANHEADER
+df02 <- structure(list(
+  KOBLID = 21L, FILID = 16L, FILGRUPPE = "TEST02",
+  FILNAVN = "testpath/test02.xlsx", IBRUKTIL = structure(253370764800, class = c(
+    "POSIXct",
+    "POSIXt"
+  ), tzone = "UTC"), LESID = 19L, FILGRUPPE = "TEST02",
+  INNLESARG = "1 = AGE", MANHEADER = "3,5=AGE", GEO = "geo",
+  AAR = "year", KJONN = "gender", ALDER = "age", UTDANN = "edu",
+  LANDBAK = "ethnic", VAL = "sum", EXTRA = NA_character_
+), class = "data.frame", row.names = c(
+  NA,
+  -1L
+))
