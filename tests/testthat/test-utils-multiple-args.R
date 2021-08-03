73c6b71c2ceb277e8fc3eb8414ab06968f2a4f5b
YBK <ybkamaleri@users.noreply.github.com>
2021-08-03 21:55:00 +0200

    Test: Use the updated arg name

--- tests/testthat/test-utils-multiple-args.R
+++ tests/testthat/test-utils-multiple-args.R
@@ -12,8 +12,8 @@ test_that("Get input column", {
   txt3 <- find_column_multi(txt1)
   txt4 <- list(header = TRUE, ark = "Sheet1")
 
-  expect_equal(find_column_multi(df = df01, col = "INNLESARG", sep = ","), c("header=TRUE", "skip=TRUE", "ark=Sheet1"))
-  expect_equal(find_column_multi(df = txt2, sep = ":"), c("header=TRUE", "sep=,"))
+  expect_equal(find_column_multi(spec = df01, col = "INNLESARG", sep = ","), c("header=TRUE", "skip=TRUE", "ark=Sheet1"))
+  expect_equal(find_column_multi(spec = txt2, sep = ":"), c("header=TRUE", "sep=,"))
 
   expect_equal(find_column_multi_input(txt3), txt4)
 
