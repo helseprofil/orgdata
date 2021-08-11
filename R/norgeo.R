2b1a761bb26610a3304aad15fbf2c8aa1c538c31
YBK <ybkamaleri@users.noreply.github.com>
2021-08-11 13:10:34 +0200

    Feat: Can change table name and warn if overwrite

--- R/norgeo.R
+++ R/norgeo.R
@@ -2,13 +2,14 @@
 #' @description Create a database for geographical codes to aggregate
 #'   data accordingly. Implementation of this function is base on [norgeo::cast_geo()]
 #'   function in \href{https://helseprofil.github.io/norgeo/}{norgeo} package.
-#' @param year Year for geographical codes
-#' @param write Write table `tblGeo` to the `orgdata.geo` database. It will overwrite
-#'    the `tblGeo` if it already exists
-#' @param append Append the data to an existing `tblGeo` in the `orgdata.geo`
+#' @param year Year for the valid geographical codes
+#' @param write Write table to the `orgdata.geo` database. It will overwrite
+#'    the table if it already exists
+#' @param append Append the data to an existing table in the `orgdata.geo`
+#' @param table Table name to be created in the database. Default is `tblGeo`
 #' @importFrom norgeo cast_geo
 #' @export
-do_norgeo <- function(year = NULL, write = FALSE, append = FALSE){
+do_norgeo <- function(year = NULL, append = FALSE, write = FALSE, table = "tblGeo"){
   is_null(year)
 
   geoFile <- is_path_db(getOption("orgdata.geo"), check = TRUE)
@@ -16,17 +17,30 @@ do_norgeo <- function(year = NULL, write = FALSE, append = FALSE){
   on.exit(geo$db_close(), add = TRUE)
 
   geo$tblvalue <- norgeo::cast_geo(year = year)
-  geo$tblname <- "tblGeo"
+  geo$tblname <- table
 
+  write <- is_write(write, table, geo$dbconn)
   if (write){
     geo$db_write(write = write)
-    message("Write table `tblGeo` is completed. \n", geoFile)
+    message("Write table `", table, "` is completed in: \n", geoFile)
   }
 
   if (append){
     geo$db_write(append = append)
-    message("Append data to `tblGeo` is completed. \n", geoFile)
+    message("Append data to `", table, "` is completed in: \n", geoFile)
   }
 
   invisible(geo$tblvalue)
+}
+
+## Helper -----------------------------------------------------
+# Warn user if table exists incase it's a mistake
+is_write <- function(write, table, con){
+
+  tblExist <- DBI::dbExistsTable(conn = con, name = table)
+  if (isTRUE(write) && isTRUE(tblExist)){
+    msgs <- sprintf("Table `%s` exists. Do you want to overwrite?", table)
+    write <- utils::askYesNo(msg = msgs)
+  }
+  return(write)
 }
