b82197f212669c8125187da14c35065290b46aeb
YBK <ybkamaleri@users.noreply.github.com>
2021-08-12 16:56:07 +0200

    Misc: Delete duplicated files

--- R/org-process.R
+++ R/org-process.R
@@ -1,7 +1,6 @@
 #' @keywords internal
 #' @title Process Raw Data
 #' @description Implement the specifications to the raw data and deliver the output
-#'   to the enclosing environment.
 #' @param file File rawdata
 #' @param filespec Specification for a file from `tbl_Innlesing`
 #' @param fgspec Specification for a file group
@@ -9,20 +8,23 @@
 #' @param verbose Make processes explicit. Default is FALSE
 #' @return A dataset with `data.table` format
 
-is_org_process <- function(file, filespec, fgspec, con, verbose = getOption("orgdata.verbose")) {
+is_org_process <- function(file,
+                           filespec,
+                           fgspec,
+                           con,
+                           verbose = getOption("orgdata.verbose")) {
   dt <- read_file(file = file)
 
   colSpec <- get_column_standard(spec = filespec)
-  dt <- do_column_standard(dt = dt, spec = colSpec)
+  dt <- do_column_standard(dt, colSpec)
   ## TODO Any extra args for file specific from INNLESARG
 
   splitSpec <- get_split(spec = fgspec)
   dt <- do_split(dt = dt, split = splitSpec)
 
-  yrSpec <- get_year(spec = filespec, con = con)
-  dt <- do_year(dt = dt, year = yrSpec)
+  yrSpec <- get_year(filespec, con)
+  dt <- do_year(dt, yrSpec)
 
   manSpec <- get_manheader(spec = filespec)
-  dt <- do_manheader(dt = dt, manspec = manSpec)
-
+  dt <- do_manheader(dt, manSpec)
 }
