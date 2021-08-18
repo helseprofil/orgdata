c652cd0db249d4ac030fb0afa51c04e57388ebe4
YBK <ybkamaleri@users.noreply.github.com>
2021-08-18 11:01:56 +0200

    Feat: Recode geo codes

--- R/norgeo.R
+++ R/norgeo.R
@@ -9,7 +9,7 @@
 #' @param table Table name to be created in the database. Default is `tblGeo`
 #' @importFrom norgeo cast_geo
 #' @export
-read_geo <- function(year = NULL, append = FALSE, write = FALSE, table = "tblGeo"){
+geo_level <- function(year = NULL, append = FALSE, write = FALSE, table = "tblGeo"){
   is_null(year)
 
   geoFile <- is_path_db(getOption("orgdata.geo"), check = TRUE)
@@ -33,8 +33,48 @@ read_geo <- function(year = NULL, append = FALSE, write = FALSE, table = "tblGeo
   invisible(geo$tblvalue)
 }
 
+#' @title Geographical Code for Recode
+#' @description Create a table of current year geographical codes against previous
+#'  years geogprahical codes. This is used to recode the previous years codes to the
+#'  current codes. Implementation of this function is base on [norgeo::track_change()]
+#'  function.
+#' @param type Type of regional granularity ie. enumeration area (grunnkrets)
+#' @param from Starting year for the range period. Current year is the default if left empty
+#' @param to End of year for the range period. Current year is the default if left empty
+#' @inheritParams geo_level
+#' @importFrom norgeo track_change
+#' @export
+geo_recode <- function(type = c("grunnkrets", "bydel", "kommune", "fylke"),
+                       from = NULL,
+                       to = NULL,
+                       write = FALSE){
+
+  type <- match.arg(type)
+  yr <- to
+  if (is.null(to)){
+    yr <- as.integer(format(Sys.Date(), "%Y"))
+  }
+  tblName <- paste("geo", yr)
+
+  ## Conn ----------------
+  geoFile <- is_path_db(getOption("orgdata.geo"), check = TRUE)
+  geo <- KHelse$new(geoFile)
+  on.exit(geo$db_close(), add = TRUE)
+
+  geo$tblvalue <- norgeo::track_change(type = type, from = from, to = to)
+  geo$tblname <- tblName
+
+  write <- is_write(write, tblName, geo$dbconn)
+  if (write){
+    geo$db_write(write = write)
+    message("Write table `", tblName, "` is completed in: \n", geoFile)
+  }
+  invisible(geo$tblvalue)
+}
+
+
 ## Helper -----------------------------------------------------
-# Warn user if table exists incase it's a mistake
+## Warn user if table exists incase it's a mistake
 is_write <- function(write, table, con){
 
   tblExist <- DBI::dbExistsTable(conn = con, name = table)
