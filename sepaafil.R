d0f250d5d214db849deffae0d4b012e50677e8db
YBK <ybkamaleri@users.noreply.github.com>
2021-08-19 11:29:10 +0200

    Style: Update users files

--- sepaafil.R
+++ sepaafil.R
@@ -1,6 +1,15 @@
-## Installasjon av pakken
-## renv::install("helseprofil/orgdata")
-## renv::snapshot()
-renv::restore()
-
+## Aggregere filgruppe
 library(orgdata)
+op <- options()
+df <- lesorg("BEFOLKNING")
+
+## Lag CSV fil
+save_file(df, "BEFOLKNING")
+
+
+## Aggregere utvalgte filer med KOBLID
+df <- lesorg("BEFOLKNING", koblid = 13)
+df
+
+## Se raw fil med FILID
+fil <- lesfil(file = 6)
---
+++ setup.R
@@ -0,0 +1,4 @@
+## Installasjon av pakken
+## renv::install("helseprofil/orgdata")
+## renv::snapshot()
+renv::restore()
