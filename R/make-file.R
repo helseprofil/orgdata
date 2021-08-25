98f6cd09804ebed2f663fb9c5b61d7696ff0b0cd
YBK <ybkamaleri@users.noreply.github.com>
2021-08-25 22:21:57 +0200

    Style: Ensure active is TRUE when exist make_file

--- R/make-file.R
+++ R/make-file.R
@@ -4,10 +4,9 @@
 #' @inheritParams do_aggregate
 #' @export
 make_file <- function(file, geo, val){
-  op <- options()
   options(orgdata.active = FALSE)
   dt <- read_file(file = file)
   dt <- do_aggregate(dt, geo = geo, val = val)
-  on.exit(options(op))
+  on.exit(options(orgdata.active = TRUE))
   invisible(dt)
 }
