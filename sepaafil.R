4e4aee60c5f6be0bb5da2abc4cc10b6243294b2d
YBK <ybkamaleri@users.noreply.github.com>
2021-09-03 13:40:18 +0200

    Misc: Add more tips and tricks

--- sepaafil.R
+++ sepaafil.R
@@ -21,6 +21,15 @@ save_file(df, "BEFOLKNING")
 lesraw("BEFOLKNING", save = TRUE)
 
 
-## TIPS AND TRICKS ---------------------------
+## TIPS AND TRICKS ------------------------------------
 ## Lest bare en bestemt antall rader f.eks 5 rader
 lesfil(file = 1, nrows = 5) #for csv fil
+lesfil(file = 28, n_max = 5) #for excel fil
+
+## Sjekk kategorier evt. omkoding
+dt <- lesfil(file = 1)
+dt[, .N, keyby = landb] #kategori for landb i original data
+
+df <- lesraw("TEST01", koblid = 1)
+df[, .N, keyby = LANDB] #kategori i omkodet aggregerte data
+df[, .N, keyby = LANDF]
