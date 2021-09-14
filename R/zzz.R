88ed177d26bbd52e5cd094146829fc9866b2091e
YBK <ybkamaleri@users.noreply.github.com>
2021-09-14 13:03:43 +0200

    Style: Add VALS and TABS to options

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,3 @@
-## Additional options specific to orgdata
-## use getOption("orgdata.folder.db") to get default folder
-## or options(orgdata.db = "dbfile.accdb") to change the filename
-
-
 ## Path for different operating system
 OS <- Sys.info()["sysname"]
 sysDrive <- switch(OS,
@@ -19,10 +14,18 @@ opt.orgdata <- list(
   orgdata.db = "raw-khelse_BE.accdb",
   orgdata.geo = "geo-koder.accdb",
   orgdata.verbose = FALSE,
-  orgdata.columns = c("GEO", "AAR", "KJONN", "ALDER", "UTDANN", "LANDBAK", "VAL"),
-  orgdata.int = c("GEO", "AAR", "KJONN", "ALDER", "VAL"),
   orgdata.aggregate = TRUE,
   orgdata.debug = FALSE,
+
+  ## Standard columns
+  orgdata.columns = c("GEO", "AAR", "KJONN", "ALDER", "UTDANN", "LANDBAK",
+                      "TAB1", "TAB2", "TAB3", "VAL1", "VAL2", "VAL3"),
+
+  ## Columns with integer values
+  orgdata.int = c("GEO", "AAR", "KJONN", "ALDER", "VAL"),
+
+  ## Either to change columnames to standard names or keep as it's.
+  ## Default is to change to standard
   orgdata.active = TRUE
 )
 
