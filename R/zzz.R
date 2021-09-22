81fa223bde7333a53663e6db120a31c623bdd16a
YBK <ybkamaleri@users.noreply.github.com>
2021-09-22 17:13:05 +0200

    Style: version 0.0.6

--- R/zzz.R
+++ R/zzz.R
@@ -39,5 +39,5 @@ opt.orgdata <- list(
 }
 
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("orgdata version 0.0.5 - alpha")
+  packageStartupMessage("orgdata version 0.0.6 - alpha")
 }
