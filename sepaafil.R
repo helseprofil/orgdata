ca435e6060e4080e508527bb6a0ac15a085a44d1
YBK <ybkamaleri@users.noreply.github.com>
2021-09-30 14:13:27 +0200

    Feat: Update lock.file

--- sepaafil.R
+++ sepaafil.R
@@ -2,33 +2,36 @@
 library(orgdata)
 
 ## Les original fil som det er ved å velge FILID
-df <- lesfil(1)
+df <- les_fil(1)
 
 ## Aggregere utvalgte filer med KOBLID
-df <- lesraw("BEFOLKNING", koblid = 13)
+df <- lag_fil("BEFOLKNING", koblid = 13)
 
 ## Les filen og bruk oppsettet i Access
-df <- lesraw("BEFOLKNING")
+df <- lag_fil("BEFOLKNING")
 
 ## Lagre CSV fil
 save_file(df, "BEFOLKNING")
 
 ## Evt. hvis alle filer har vært kontrollert
-lesraw("BEFOLKNING", save = TRUE)
+lag_fil("BEFOLKNING", save = TRUE)
 
 
 ## TIPS AND TRICKS ------------------------------------
 ## Lest bare en bestemt antall rader f.eks 5 rader
-lesfil(file = 1, nrows = 5) #for csv fil
-lesfil(file = 28, n_max = 5) #for excel fil
+les_fil(file = 1, nrows = 5) #for csv fil
+les_fil(file = 28, n_max = 5) #for excel fil
 
 ## Sjekk kategorier evt. omkoding
-dt <- lesfil(file = 1)
+dt <- les_fil(file = 1)
 dt[, .N, keyby = landb] #kategori for landb i original data
 
-df <- lesraw("TEST01", koblid = 1)
+df <- lag_fil("TEST01", koblid = 1)
 df[, .N, keyby = LANDB] #kategori i omkodet aggregerte data
 df[, .N, keyby = LANDF]
 
 ## Åpne hjemmeside
 website()
+
+## Sjekk versjon
+packageVersion("orgdata")
