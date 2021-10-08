5b0600e356d862d55f2681bfc93b2e5c75aa4890
YBK <ybkamaleri@users.noreply.github.com>
2021-10-08 16:52:14 +0200

    Style: Exclude '-' sql query recode codebook

--- inst/recode.sql
+++ inst/recode.sql
@@ -1,3 +1,6 @@
 select FILGRUPPE, LESID, KOL, TYPE, FRA, TIL
 from tbl_Kode
-where (FILGRUPPE = '%s' or FILGRUPPE = 'ALLE') and VERSJONTIL = #9999-01-01# and (LESID is NULL or LESID = '%s')
+where (FILGRUPPE = '%s' or FILGRUPPE = 'ALLE')
+  and VERSJONTIL = #9999-01-01#
+  and (LESID is NULL or LESID = '%s')
+  and TIL <> '-'
