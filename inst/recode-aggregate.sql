c747c7d27f8274f1eb8bfee49b5b76eb5c22f87b
YBK <ybkamaleri@users.noreply.github.com>
2021-10-11 01:09:39 +0200

    Feat: Recode and recode aggregate variables

--- inst/recode-aggregate.sql
+++ inst/recode-aggregate.sql
@@ -1,3 +1,7 @@
-select FILGRUPPE, KOL, TYPE, FRA, TIL
+select FILGRUPPE, LESID, KOL, TYPE, FRA, TIL
 from tbl_Kode
-where FILGRUPPE = '%s' and VERSJONTIL = #9999-01-01#
+where (FILGRUPPE = '%s' or FILGRUPPE = 'ALLE')
+  and VERSJONTIL = #9999-01-01#
+  and (LESID is NULL or LESID = '%s')
+  and TYPE = 'AG'
+  and TIL <> '-'
