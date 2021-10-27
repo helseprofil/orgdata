1671fe890708695bd54c2f952b60c72fa6c2c86d
YBK <ybkamaleri@users.noreply.github.com>
2021-10-27 15:42:52 +0200

    Test for sql query

--- tests/testthat/test-sql-spec.R
+++ tests/testthat/test-sql-spec.R
@@ -10,3 +10,15 @@ test_that("Check missing arg and SQL error", {
 
   expect_error(find_spec(file, value), "Argument for `con` is missing")
 })
+
+
+test_that("SQL query", {
+
+  sqQry <- "select FILGRUPPE, LESID, KOL, TYPE, FRA, TIL\nfrom tbl_Kode\nwhere (FILGRUPPE = 'TEST' or FILGRUPPE = 'ALLE')\n  and VERSJONTIL = #9999-01-01#\n  and (LESID is NULL or LESID = 'ver01')\n  and TYPE = 'AG'\n  and TIL <> '-'"
+  sqOpp <- "SELECT * FROM tblGeo\nWHERE validTo='1990' AND level='GRK'"
+
+
+  ## TEST
+  expect_equal(is_query(file = "recode-aggregate.sql", char = "TEST", char2 = "ver01"), sqQry)
+  expect_equal(is_query(file = "geo-code.sql", char = "GRK", char2 = 1990, opposite = TRUE), sqOpp)
+})
