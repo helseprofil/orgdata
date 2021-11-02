b1b219c3cf1883583fff23a465cc72d8d405f84f
YBK <ybkamaleri@users.noreply.github.com>
2021-11-02 12:10:36 +0100

    Fix #100 grunnkrets code ends with 00

--- tests/testthat/test-norgeo-recode.R
+++ tests/testthat/test-norgeo-recode.R
@@ -16,3 +16,33 @@ test_that("Add grunnkrets code", {
   expect_equal(is_grunnkrets(dgrk), out)
 
 })
+
+
+test_that("Grunnkrets ends with 00", {
+
+  ## Data ---------
+  input <- structure(list(code = c("03010100", "03010101", "54449900", "54449999"),
+                          name = c("Sentrum 1", "Sentrum 1  - Rode 1", "Uoppgitt delområde", "Uoppgitt grunnkrets"),
+                          validTo = c("2021", "2021", "2021", "2021"),
+                          level = c("grunnkrets", "grunnkrets", "grunnkrets", "grunnkrets"),
+                          grunnkrets = c("03010100", "03010101", "54449900", "54449999"),
+                          kommune = c(NA, "0301", NA, "5444"), fylke = c(NA, "03", NA, "54"),
+                          bydel = c(NA, "030116", NA, NA)),
+                     row.names = c(NA, -4L),
+                     class = c("data.table", "data.frame"), sorted = "code")
+
+  output <- structure(list(code = c("03010100", "03010101", "54449900", "54449999"),
+                           name = c("Sentrum 1", "Sentrum 1  - Rode 1", "Uoppgitt delområde", "Uoppgitt grunnkrets"),
+                           validTo = c("2021", "2021", "2021", "2021"),
+                           level = c("grunnkrets", "grunnkrets", "grunnkrets", "grunnkrets"),
+                           grunnkrets = c("03010100", "03010101", "54449900", "54449999"),
+                           kommune = c("0301", "0301", "5444", "5444"),
+                           fylke = c("03", "03", "54", "54"),
+                           bydel = c("030116", "030116", NA, NA)),
+                      row.names = c(NA, -4L),
+                      class = c("data.table", "data.frame"), sorted = "code")
+
+  ## Test
+  expect_equal(is_grunnkrets_00(input), output)
+
+})
