f7f768e36d709deeabfc1e6dba65ce51ab01197e
YBK <ybkamaleri@users.noreply.github.com>
2021-12-17 14:29:50 +0100

    Style: Create reshape wide all possible dimensions

--- R/reshape-wide.R
+++ R/reshape-wide.R
@@ -6,6 +6,10 @@
 #' @family reshape functions
 #' @export
 do_reshape_wide <- function(dt = NULL, respec = NULL){
+
+  is_debug()
+  is_null(respec)
+
   dt <- copy(dt)
   resCol <- respec$rescol
   resVal <- respec$resval
@@ -55,12 +59,45 @@ get_reshape_wide_spec <- function(dt = NULL, group = NULL, con = NULL, spec = NU
   }
 
   ## TODO Delete or exclude column that should not be included as in RESHAPE_ID
-
-  resCol <- spec[["RESHAPE_KOL"]]
-  resVal <- spec[["RESHAPE_VAL"]]
-  valCols <- as.character(unique(dt[[resCol]]))
+  resCol <- find_column_multi(spec, "RESHAPE_KOL")
+  resVal <- find_column_input(spec, "RESHAPE_VAL")
+  valCols <- is_reshape_wide_cols(dt, resCol)
 
   return(list(rescol = resCol,
               resval = resVal,
               valcols = valCols))
+}
+
+## Helper ---------------------
+## All possible combination of unique vectors to create value columns
+## from when dt is turned to wide especially with multiple reshape columns
+is_reshape_wide_cols <- function(dt, col){
+  # col - Value columns in dataset when turn to wide
+
+  ## TODO Need to refactor this! Too many repeatition!
+  if (length(col) == 3){
+    col1 <- col[1]
+    col2 <- col[2]
+    col3 <- col[3]
+    colVal1 <- as.character(unique(dt[[col1]]))
+    colVal2 <- as.character(unique(dt[[col2]]))
+    colVal3 <- as.character(unique(dt[[col3]]))
+    idx <- data.table::CJ(colVal1, colVal2, colVal3)
+    idx[, cols := paste0(colVal1, ";", colVal2, ";", colVal3)]
+    valCols <- idx[["cols"]]
+  }
+
+  if (length(col) == 2){
+    col1 <- col[1]
+    col2 <- col[2]
+    colVal1 <- as.character(unique(dt[[col1]]))
+    colVal2 <- as.character(unique(dt[[col2]]))
+    idx <- data.table::CJ(colVal1, colVal2)
+    idx[, cols := paste0(colVal1, ";", colVal2)]
+    valCols <- idx[["cols"]]
+  } else {
+    valCols <- as.character(unique(dt[[col]]))
+  }
+
+  return(valCols)
 }
