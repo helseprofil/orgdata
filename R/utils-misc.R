6640c71f35915d33b7a0795c106544e09bf89cc5
YBK <ybkamaleri@users.noreply.github.com>
2021-12-18 17:14:10 +0100

    Feat: Wrapper for debugging options

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -18,6 +18,8 @@ reset_options <- function(){
 
 #' @title Debugging options
 #' @description A wrapper for debugging options.
+#' @param opt Debug options
+#' @param val Value options
 #' @details Options available:
 #'   - "fun"       : Activating options `orgdata.debug`
 #'   - "nrow"      : For `orgdata.debug.nrow`. Default value is 20 if `val` argument is missing
@@ -38,4 +40,19 @@ debug_opt <- function(opt = c("fun", "nrow", "row", "aggregate", "geo"), val = N
          row = options(orgdata.debug.row = ifelse(is.null(val), 1:50, val)),
          aggregate = options(orgdata.debug.aggregate = TRUE),
          geo = options(orgdata.debug.geo = TRUE))
+}
+
+
+#' @title Emoji
+#' @description Use emoji for fun :smile:
+#' @param x Emoji to choose ie. thumb, smile or sad
+#' @examples emoji("smile")
+#' @export
+emoji <- function(x = c("thumb", "smile", "sad")){
+  x <- match.arg(x)
+  switch(x,
+         thumb = options(orgdata.emoji = "thumb"),
+         smile = options(orgdata.emoji = "smile"),
+         sad = options(orgdata.emoji = "sad")
+         )
 }
