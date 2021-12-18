b0497f1ab13272a9a983e10a131970f94a32e720
YBK <ybkamaleri@users.noreply.github.com>
2021-12-18 17:41:57 +0100

    Fix #191 add wrapper for debugging options (#196)
    * Feat: A wrapper for debug options
    
    * Feat: Wrapper for debugging options
    
    * Doc: Wrapper for debug options and emoji

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -15,3 +15,49 @@ website <- function(url = NULL){
 reset_options <- function(){
   options(opt.orgdata)
 }
+
+
+#' @export
+#' @rdname reset_options
+reset_opt <- reset_options
+
+#' @title Debugging options
+#' @description A wrapper for debugging options.
+#' @param opt Debug options
+#' @param val Value options
+#' @details Options available:
+#'   - "fun"       : Activating options `orgdata.debug`
+#'   - "nrow"      : For `orgdata.debug.nrow`. Default value is 20 if `val` argument is missing
+#'   - "row"       : For `orgdata.debug.row`. Default value is 1:50 if `val` argument is missing
+#'   - "aggregate" : Activating `orgdata.debug.aggregate`
+#'   - "geo"       : Activating `orgdata.debug.geo`
+#' @examples
+#' \dontrun{
+#' debug_opt("geo")
+#' debug_opt("nrow", 30)
+#' }
+#' @export
+debug_opt <- function(opt = c("fun", "nrow", "row", "aggregate", "geo"), val = NULL){
+  opt <- match.arg(opt)
+  switch(opt,
+         fun = options(orgdata.debug = TRUE),
+         nrow = options(orgdata.debug.nrow = ifelse(is.null(val), 20, val)),
+         row = options(orgdata.debug.row = ifelse(is.null(val), 1:50, val)),
+         aggregate = options(orgdata.debug.aggregate = TRUE),
+         geo = options(orgdata.debug.geo = TRUE))
+}
+
+
+#' @title Emoji
+#' @description Use emoji for fun :smile:
+#' @param x Emoji to choose ie. thumb, smile or sad
+#' @examples emoji("smile")
+#' @export
+emoji <- function(x = c("thumb", "smile", "sad")){
+  x <- match.arg(x)
+  switch(x,
+         thumb = options(orgdata.emoji = "thumb"),
+         smile = options(orgdata.emoji = "smile"),
+         sad = options(orgdata.emoji = "sad")
+         )
+}
