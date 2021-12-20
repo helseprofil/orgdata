d32a769e3e7c46f3240d80339da9e76c317f7517
YBK <ybkamaleri@users.noreply.github.com>
2021-12-20 18:14:59 +0100

    Closed #198 give feedback with make_filegroups

--- R/make-group.R
+++ R/make-group.R
@@ -24,9 +24,33 @@ make_filegroups <- function(...){
     fgp <- sapply(as.list(dots), deparse)
   }
 
+  fgpKO <- list()
+  fgpOK <- list()
+
   for (i in fgp){
     i <- trimws(i)
-    make_file(i, save = TRUE)
+
+    FGP <- tryCatch({make_file(i, save = TRUE)},
+                    error = function(err) err)
+
+    if (is(FGP, "error")){
+      fgpKO[i] <- i
+      next
+    } else {
+      fgpOK[i] <- i
+    }
+  }
+
+  is_line_short()
+  msgOK <- paste0("Done ", length(fgpOK), " group(s):")
+  is_color_txt(fgpOK, msg = msgOK, type = "note")
+  is_color_txt(x = "`log$ok`", msg = "Check all the filegroups with:")
+
+  if (length(fgpKO) > 0) {
+    is_line_short()
+    msgKO <- paste0("Error ", length(fgpKO), " group(s):")
+    is_color_txt(fgpKO, msg = msgKO, type = "error")
+    is_color_txt(x = "`log$ko`", msg = "Check all the filegroups with:")
   }
 }
 
