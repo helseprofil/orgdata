151bb14895dc08188e88d0cc27e1e7b8ea0f1c99
YBK <ybkamaleri@users.noreply.github.com>
2021-12-29 21:00:05 +0100

    Test: Connection test

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -3,8 +3,11 @@ test_that("Connection to DB", {
   geofile <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/geo-koder.accdb"
   khfile <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/raw-database_BE.accdb"
 
+  skip_on_ci()
+  skip_on_covr()
+  skip_on_cran()
   expect_equal(is_conn_db(db = "geo", .test = TRUE, check = FALSE), geofile)
   expect_equal(is_conn_db(db = "kh", .test = TRUE, check = FALSE), khfile)
-  expect_equal(is_conn_db(.test = TRUE, check = FALSE), "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/raw-database_BE.accdb")
+  expect_equal(is_conn_db(.test = TRUE, check = FALSE), khfile)
 
 })
