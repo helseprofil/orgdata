f1905dc479407187c748c396b17e66bace9936f7
YBK <ybkamaleri@users.noreply.github.com>
2021-12-29 20:56:37 +0100

    Test: connection with db file

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -5,6 +5,6 @@ test_that("Connection to DB", {
 
   expect_equal(is_conn_db(db = "geo", .test = TRUE, check = FALSE), geofile)
   expect_equal(is_conn_db(db = "kh", .test = TRUE, check = FALSE), khfile)
-  expect_equal(is_conn_db(.test = TRUE, check = FALSE), khfile)
+  expect_equal(is_conn_db(.test = TRUE, check = FALSE), "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/raw-database_BE.accdb")
 
 })
