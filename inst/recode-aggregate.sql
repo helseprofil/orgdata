abac75a109741e00d6458684f2821d24b60461a9
YBK <ybkamaleri@users.noreply.github.com>
2021-12-31 00:56:19 +0100

    Close #213 use filegroup spec for recode aggregate

--- inst/recode-aggregate.sql
+++ inst/recode-aggregate.sql
@@ -2,6 +2,5 @@ select FILGRUPPE, LESID, KOL, TYPE, FRA, TIL
 from tbl_KodeBok
 where (FILGRUPPE = '%s' or FILGRUPPE = 'ALLE')
   and VERSJONTIL = #9999-01-01#
-  and (LESID is NULL or LESID = '%s')
   and TYPE = 'AG'
   and TIL <> '-'
