07c44f921d8da7c0c3ba31ebf2b4c9963822c6fb
YBK <ybkamaleri@users.noreply.github.com>
2022-01-13 19:50:16 +0100

    Test: Reshape wide and back to long with TABS

--- tests/testthat/test-reshape-wide.R
+++ tests/testthat/test-reshape-wide.R
@@ -1,46 +1,50 @@
-test_that("Reshape wide", {
+test_that("Reshape wide tab 1", {
 
-  ## DATA ---
-  spec <- list(rescol = "TAB1", resval = "VAL1", valcols = c("2", "1"))
+  dtTab1 <- readRDS(system.file("testdata", "wide-tab01.rds", package = "orgdata"))
+  dtTabSpec <- readRDS(system.file("testdata", "wide-tab01-spec.rds", package = "orgdata"))
+  dtTabOut <- readRDS(system.file("testdata", "wide-tab01-out.rds", package = "orgdata"))
 
-  spec2 <- list(rescol = c("TAB1", "TAB2"), resval = "VAL1",
-                widecols = c("1;5", "1;8", "2;5", "2;8", "3;5", "3;8", "4;5", "4;8", "5;5", "5;8"))
+  expect_equal(do_reshape_wide(dtTab1, dtTabSpec), dtTabOut)
 
-  col <- c("TAB1", "TAB2")
-  cols <- c("1;5", "2;5", "3;5", "3;8", "4;8", "5;8")
-
-  dt <- structure(list(AAR = c(2007L, 2007L),
-                       GEO = c(11510101L, 11510101L),
-                       KJONN = c(1L, 1L), UTDANN = c(2L, 2L), LANDSSB = c(0L, 0L),
-                       TAB1 = 2:1, TAB2 = c(5L, 5L), VAL1 = c(1L, 1L),
-                       VAL2 = c(2L, 2L), LANDBAK = c("0", "0"), INNVKAT = c("0", "0")),
-                  row.names = c(NA, -2L), class = c("data.table", "data.frame"))
-  output <- structure(list(AAR = 2007L, GEO = 11510101L, KJONN = 1L, UTDANN = 2L,
-                           LANDSSB = 0L, TAB2 = 5L, VAL2 = 2L, LANDBAK = "0", INNVKAT = "0",
-                           `1` = 1L, `2` = 1L),
-                      row.names = c(NA, -1L), class = c("data.table", "data.frame"),
-                      sorted = c("AAR", "GEO", "KJONN", "UTDANN", "LANDSSB", "TAB2",
-                                 "VAL2", "LANDBAK", "INNVKAT"))
-
-  wideDT <- readRDS(system.file("testdata", "dt-wide.rds", package = "orgdata"))
-  wideOut <- readRDS(system.file("testdata", "dt-wide-out.rds", package = "orgdata"))
-  wideColDT <- readRDS(system.file("testdata", "dt-reshape-col.rds", package = "orgdata"))
-
-  longDT <- readRDS(system.file("testdata", "dt-long.rds", package = "orgdata"))
-  longOut <- readRDS(system.file("testdata", "dt-long-out.rds", package = "orgdata"))
-
-  ## TEST ----
-  expect_equal(do_reshape_wide(dt = dt, respec = spec), output)
-  expect_equal(do_reshape_wide(dt = wideDT, respec = spec2), wideOut)
-  expect_equal(is_reshape_wide_cols(dt = wideDT, col = col), spec2[["widecols"]])
-  expect_equal(is_reshape_wide_cols(dt = wideColDT, col = "TAB1"), c("1", "3", "4", "2", "5"))
-  expect_equal(do_reshape_long(dt = longDT, resval = "VAL1", rescol = col, widecols = cols), longOut)
 })
 
-test_that("Reshape wide bracket", {
-  b1 <- "^(.*)"
-  b3 <- "^(.*);(.*);(.*)"
 
-  expect_equal(is_bracket(1), b1)
-  expect_equal(is_bracket(3), b3)
+test_that("Reshape wide tab 2", {
+
+  dtTab <- readRDS(system.file("testdata", "wide-tab02.rds", package = "orgdata"))
+  dtTabSpec <- readRDS(system.file("testdata", "wide-tab02-spec.rds", package = "orgdata"))
+  dtTabOut <- readRDS(system.file("testdata", "wide-tab02-out.rds", package = "orgdata"))
+
+  expect_equal(do_reshape_wide(dtTab, dtTabSpec), dtTabOut)
+
+})
+
+test_that("Reshape wide long tab 1", {
+
+  dtTab1 <- readRDS(system.file("testdata", "wide-long-tab1.rds", package = "orgdata"))
+  dtTabSpec <- readRDS(system.file("testdata", "wide-long-tab1-spec.rds", package = "orgdata"))
+  dtTabOut <- readRDS(system.file("testdata", "wide-long-tab1-out.rds", package = "orgdata"))
+
+  expect_equal(do_reshape_long(dtTab1, dtTabSpec), dtTabOut)
+
+})
+
+test_that("Reshape wide long tab 2", {
+
+  dtTab <- readRDS(system.file("testdata", "wide-long-tab2.rds", package = "orgdata"))
+  dtTabSpec <- readRDS(system.file("testdata", "wide-long-tab2-spec.rds", package = "orgdata"))
+  dtTabOut <- readRDS(system.file("testdata", "wide-long-tab2-out.rds", package = "orgdata"))
+
+  expect_equal(do_reshape_long(dtTab, dtTabSpec), dtTabOut)
+
+})
+
+test_that("Reshape wide spec tab 1", {
+
+  dt <- readRDS(system.file("testdata", "wide-tab01.rds", package = "orgdata"))
+  dtSpec <- readRDS(system.file("testdata", "wide-file-spec.rds", package = "orgdata"))
+  dtSpecOut <- readRDS(system.file("testdata", "wide-file-spec-out.rds", package = "orgdata"))
+
+  expect_equal(get_reshape_wide_spec(dt = dt, spec = dtSpec), dtSpecOut)
+
 })
