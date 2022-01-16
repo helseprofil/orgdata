4fd8fa855db51b9237f0966085c3b496e83777bf
YBK <ybkamaleri@users.noreply.github.com>
2022-01-16 16:44:18 +0100

    Give instruction to upgrade

--- update.R
+++ update.R
@@ -1,3 +1,5 @@
 ## Kjør alt nedenfor for å installere orgdata
 if(!requireNamespace("remotes")) install.packages("remotes")
 remotes::install_github("helseprofil/orgdata")
+
+## Svar JA eller ALL hvis det er spørsmål om å oppdatere pakkene
