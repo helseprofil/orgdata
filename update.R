f3509f379c8284b5203727f6e41946931961a2e8
YBK <ybkamaleri@users.noreply.github.com>
2022-01-17 11:07:08 +0100

    Edit ver 0.5.4

--- update.R
+++ update.R
@@ -1,5 +1,5 @@
 ## Kjør alt nedenfor for å installere orgdata
 if(!requireNamespace("remotes")) install.packages("remotes")
-remotes::install_github("helseprofil/orgdata")
+remotes::install_github("helseprofil/orgdata", force = TRUE)
 
 ## Svar JA eller ALL hvis det er spørsmål om å oppdatere pakkene
