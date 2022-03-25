ef7aac962cc6ffbb0159681a4a970217d50ece17
Yusman Kamaleri <ybkamaleri@gmail.com>
2022-03-25 11:53:00 +0100

    Doc: Update website

--- tests/testthat/test-connection.R
+++ tests/testthat/test-connection.R
@@ -1,7 +1,7 @@
 test_that("Connection to DB", {
 
-  geofile <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/geo-koder.accdb"
-  khfile <- "F:/Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/raw-database_BE.accdb"
+  geofile <- file.path(os_drive(), "Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/geo-koder.accdb" )
+  khfile <- file.path(os_drive(), "Forskningsprosjekter/PDB 2455 - Helseprofiler og til_/PRODUKSJON/STYRING/raw-khelse/raw-database_BE.accdb" )
 
   skip_on_ci()
   skip_on_covr()
