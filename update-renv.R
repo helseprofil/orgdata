d6a07e81f7a0b6a12b4358a17b9e87877879a70f
YBK <ybkamaleri@users.noreply.github.com>
2022-04-20 17:26:54 +0200

    Misc: Update packages

--- update-renv.R
+++ update-renv.R
@@ -2,7 +2,8 @@ install.packages("renv")
 library(renv)
 
 renv::install("helseprofil/orgdata@main")
-renv::install("norgeo")
+## renv::install("norgeo")
+## renv::install("rlang")
 renv::snapshot()
 
 renv::status()
