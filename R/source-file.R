19249dedb42cec6d37cfc32162fceb79b3c1c682
YBK <ybkamaleri@gmail.com>
2022-09-14 15:53:34 +0200

    DEV: Read external Stata or R files

---
+++ R/source-file.R
@@ -0,0 +1,16 @@
+#' Source R or Stata files
+#' @description Sourcing R or Stata files via `EXTRA` column. All files should
+#'   be in *source* repo in Helseprofil Github site and saved in their
+#'   respective folders. **This function is not implemented yet.**
+#' @param dt Dataset
+#' @param extra Arguments from EXTRA column in Access
+
+do_source_file <- function(dt, extra){
+
+}
+
+
+## Helper -----------------------
+is_file_r <- function(dt, file){}
+
+is_file_stata <- function(dt, file){}
