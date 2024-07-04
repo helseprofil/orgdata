1c8f3869f6977995b1d9a26ddd1ef7183954c244
Vegard Lysne <vegard.lysne@helsedir.no>
2024-07-04 14:16:12 +0200

    fix tests

--- tests/testthat/test-make-file.R
+++ tests/testthat/test-make-file.R
@@ -1,16 +1,16 @@
-test_that("Make file simple", {
-
-  testthat::skip_on_covr()
-  testthat::skip_on_ci()
-  testthat::skip_on_cran()
-  skip_if_check()
-  ## simpleDT <- readRDS(system.file("testdata_dev", "make-file-dt.rds", package = "orgdata"))
-  ## leseOUT <- readRDS(system.file("testdata_dev", "make-file2-dt.rds", package = "orgdata"))
-  ## debug_opt("nrow", 500)
-  ## expect_equal(make_file("ENPERSON", 272, year = 2021), simpleDT)
-  ## expect_equal(make_file("LESEFERD", 144, year = 2021), leseOUT)
-  reset_opt()
-})
+# test_that("Make file simple", {
+# 
+#   testthat::skip_on_covr()
+#   testthat::skip_on_ci()
+#   testthat::skip_on_cran()
+#   skip_if_check()
+#   ## simpleDT <- readRDS(system.file("testdata_dev", "make-file-dt.rds", package = "orgdata"))
+#   ## leseOUT <- readRDS(system.file("testdata_dev", "make-file2-dt.rds", package = "orgdata"))
+#   ## debug_opt("nrow", 500)
+#   ## expect_equal(make_file("ENPERSON", 272, year = 2021), simpleDT)
+#   ## expect_equal(make_file("LESEFERD", 144, year = 2021), leseOUT)
+#   reset_opt()
+# })
 
 test_that("Select files", {
 
